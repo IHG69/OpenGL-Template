49a6f47564b90c55f51ac361168890334a851899
IHG69 <felyxhjoo@proton.me>
2025-10-01 14:29:51 -0300

    Changed shaders version to 4.6

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,7 @@
 #include <GLFW/glfw3.h>
 
 // Vertex Shader Source
-const char* vertexShaderSource = "#version 330 core\n"
+const char* vertexShaderSource = "#version 460 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
@@ -18,7 +18,7 @@ const char* vertexShaderSource = "#version 330 core\n"
 "}\0";
 
 // Fragment Shader Source
-const char* fragmentShaderSource = "#version 330 core\n"
+const char* fragmentShaderSource = "#version 460 core\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
